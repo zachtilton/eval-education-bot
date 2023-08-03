f64ebe54182e045b3080ddf91dbb969bb9a05a6a
zachtilton <zdtilton@gmail.com>
2023-08-03 12:56:46 -0400

    attempting to update config and env variables; pinecone and openAI API keys

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -6,8 +6,8 @@ if (!process.env.PINECONE_INDEX_NAME) {
   throw new Error('Missing Pinecone index name in .env file');
 }
 
-const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? 'evaluation';
+const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? 'eval-chatbots';
 
-const PINECONE_NAME_SPACE = 'evaluation education'; //namespace is optional for your vectors
+const PINECONE_NAME_SPACE = 'eval-chatbots'; //namespace is optional for your vectors
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
